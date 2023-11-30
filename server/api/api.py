75f9c8d952417c61bcdada816c820bde4ed18b65
Thibault <t.lantzener@gmail.com>
2023-11-30 12:05:34 +0100

    initialisation du projet

---
+++ server/api/api.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource
+import pandas as pd
+import numpy as np
+import random
+
+
+app = Flask(__name__)
+api = Api(app)
+
+users = [{
+     "user_id": "1",
+     "username": "Zidane"
+}]
+
+@app.route("/", methods=['GET'])
+def index():
+     return jsonify("Hello World"), 200 
+
+@app.route("/get-user/<user_id>", methods=["GET"])
+def get_user(user_id):
+    user = [users for user in users if user['user_id'] == user_id]
+    user_data = {
+        "user": user[0],     
+    }
+
+    extra = request.args.get("extra")
+    if extra:
+         user_data["extra"] = extra
+
+    return jsonify(user_data), 200
+
+@app.route("/create-user", methods=["POST"])
+def create_user():
+    data = {"user_id": request.json('user_id'),
+            "username": request.json('username')
+            }
+
+    users.append(data)
+    return jsonify(data), 201
+
+
+if __name__ == '__main__':
+     #app.run(host='0.0.0.0', port=5000)
+     app.run(debug=True)
+ 
