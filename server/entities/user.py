3a0c95bf206d54b0393f2d51b631adb24df581b6
Thibault <t.lantzener@gmail.com>
2023-12-07 11:59:56 +0100

    ajout d'une classe user (backend)

---
+++ server/entities/user.py
@@ -0,0 +1,13 @@
+class User:
+    userId = 0
+    username = ''
+    email = ''
+    password = ''
+
+    def __init__(self, userId, username, email, password):
+        self.userId = userId
+        self.username = username
+        self.email = email
+        self.password = password
+
+    
