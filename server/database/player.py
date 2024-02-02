5d0dfaea108ec9166248cbed3cdcd69f3ed08bd8
Thibault <t.lantzener@gmail.com>
2024-02-02 12:17:13 +0100

    modifs méthodes add et get Player (database/player.py)

--- server/database/player.py
+++ server/database/player.py
@@ -1,10 +1,15 @@
 from database.connect import db
 from entities.player import Player
+from dataset import Table
 
-players_table = db['players']
+players_table = Table(db, 'players')  # Table(db['players'])
 
 def addPlayer(player):
+    found = getPlayer(player.id_player)
+    if not found:
+        players_table.insert(dict(player))
     return 0
 
-def getPlayer(id):
-    return 0
+def getPlayer(id_player):
+    player = players_table.find_one(id=id_player)
+    return player
