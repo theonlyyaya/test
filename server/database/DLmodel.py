4aeefe4dc50c3bae52110eef875d3b085ab9f779
Thibault <t.lantzener@gmail.com>
2024-02-08 10:23:30 +0100

    modifs BDD

--- server/database/DLmodel.py
+++ server/database/DLmodel.py
@@ -1,4 +1,8 @@
+from dataset import Table
 from database.connect import db
 from entities.model import Model
 
-models_table = db['dlmodels']
+models_table = Table(db, 'dlmodels')
+
+def addModel():
+    return 0
--- server/database/player.py
+++ server/database/player.py
@@ -2,7 +2,7 @@ from database.connect import db
 from entities.player import Player
 from dataset import Table
 
-players_table = Table(db, 'players')  # Table(db['players'])
+players_table = Table(db, 'players')
 
 def addPlayer(player):
     found = getPlayer(player.id_player)
@@ -12,4 +12,26 @@ def addPlayer(player):
 
 def getPlayer(id_player):
     player = players_table.find_one(id=id_player)
+    if(player):
+        player = Player(**(player))
     return player
+
+def logIn(email, password):
+    player = players_table.find_one(email=email)
+    if(player.password == password):
+        return True
+    return False 
+
+
+
+def signIn(email, password):
+    """
+    Parameters
+    ----------
+    email,
+    password
+    """
+    player = players_table.find_one(email=email)
+    if(player):
+        return False
+    return True
