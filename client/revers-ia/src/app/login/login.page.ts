944b5e0697f4af38a3622ccaa614a8e8cdd957b4
thatwasyahya <98318509+thatwasyahya@users.noreply.github.com>
2024-04-04 09:48:08 +0000

    login and back arrow

--- client/revers-ia/src/app/login/login.page.ts
+++ client/revers-ia/src/app/login/login.page.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+// login.page.ts
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  email: string = '';
+  password: string = '';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  login() {
+    // Ici, vous pouvez ajouter votre logique d'authentification
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+
+    // Supposons que l'authentification est réussie, vous pouvez rediriger l'utilisateur
+    // Remplacez '/dashboard' par le chemin de votre page de dashboard ou page principale
+    this.router.navigate(['/dashboard']);
   }
 
+  navigateToSignup() {
+    // Navigue vers la page d'inscription
+    this.router.navigate(['/signup']);
+  }
 }
--- client/revers-ia/src/app/signup/signup.page.ts
+++ client/revers-ia/src/app/signup/signup.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CountryService } from '../country.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,28 +8,33 @@ import { Router } from '@angular/router';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
-
+  countries: any[] = [];
   username: string = '';
   email: string = '';
   password: string = '';
   confirmPassword: string = '';
-  age: string = '';
+  country: string = '';
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private countryService: CountryService) { }
 
   ngOnInit() {
+    this.countryService.getCountries().subscribe((data) => {
+      this.countries = data.map((country) => ({
+        name: country.name.common, // Adaptez cette ligne si la structure de données de l'API change
+        code: country.cca2
+      })).sort((a, b) => a.name.localeCompare(b.name));
+    });
   }
 
   signUp() {
-    // Vous pouvez ajouter ici la logique pour envoyer les données d'inscription à votre backend
+    // Logique d'inscription
     console.log('Username:', this.username);
     console.log('Email:', this.email);
     console.log('Password:', this.password);
-    console.log('confirmPassword:', this.confirmPassword);
-    console.log('age:', this.age);
+    console.log('Confirm Password:', this.confirmPassword);
+    console.log('Country:', this.country);
 
-    // Une fois l'inscription réussie, vous pouvez rediriger l'utilisateur vers une autre page
-    this.router.navigate(['/login']); // Remplacez '/home' par le chemin de la page où vous souhaitez rediriger l'utilisateur après l'inscription
+    // Redirection après inscription
+    this.router.navigate(['/login']); // Assurez-vous que le chemin est correct
   }
-
 }
